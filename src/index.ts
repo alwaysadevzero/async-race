352bec66512a31677b61de0fc69d381fc325319f
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-22 03:36:08 +0400

    feat: add index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(1234)
+import "./styles.css"
+import Router from "./router/router"
+
+class App {
+  constructor() {
+    const router = new Router()
+    const main = document.createElement("main")
+    document.body.appendChild(main)
+    main.classList.add("container")
+  }
+}
+
+// eslint-disable-next-line no-new
+new App()
