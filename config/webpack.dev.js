24455b388ec5c602ba07bd4ddacd4d70c16e6bfa
Anthony <alwaysadevzero@gmail.com>
2023-07-21 11:46:20 +0000

    feat: add base environment

---
+++ config/webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = (env, mode) =>
+  merge(common(env, mode), {
+    devtool: "eval-cheap-module-source-map",
+    target: "web",
+    devServer: {
+      open: true,
+      static: { directory: path.resolve(__dirname, "../dist") },
+      host: "local-ip",
+    },
+    output: {
+      asyncChunks: true,
+      filename: `[name].js`,
+      publicPath: "auto",
+      path: path.resolve(__dirname, "../dist"),
+    },
+  });
