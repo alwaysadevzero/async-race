c62d76e1c2fcd827c4916acee179113a6f4e0f8c
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-22 18:10:19 +0400

    feat: add eventEmmiter

---
+++ src/utils/eventEmmiter.ts
@@ -0,0 +1,32 @@
+import { Events } from "../enums/enum-events"
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Listener = (args: any) => void
+
+class EventEmitter {
+  private listeners: Record<string, Listener[]> = {}
+
+  public events: typeof Events = Events
+
+  public on(event: Events, listener: Listener): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = []
+    }
+    this.listeners[event].push(listener)
+  }
+
+  public off(event: Events, listener: Listener): void {
+    if (!this.listeners[event]) return
+    const callbackIndex = this.listeners[event].indexOf(listener)
+    if (callbackIndex >= 0) {
+      this.listeners[event].splice(callbackIndex, 1)
+    }
+  }
+
+  public emit(event: Events, data?: unknown): void {
+    if (!this.listeners[event]) return
+    this.listeners[event].forEach((listener) => listener(data))
+  }
+}
+
+export default new EventEmitter()
