dc0bec66762a9b04610bc522d1cdac5ff3927951
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-24 01:09:09 +0400

    refactor: add space according prettier

--- src/model/garage-model.ts
+++ src/model/garage-model.ts
@@ -1,34 +1,42 @@
-import { GarageState } from "../interfaces/garage-state.interface";
-import RaceApi from "../services/race-api";
-import { Car } from "../interfaces/car.interface";
+import { GarageState } from "../interfaces/garage-state.interface"
+import RaceApi from "../services/race-api"
+import { Car } from "../interfaces/car.interface"
 
 export default class GarageModel {
-    private api = new RaceApi()
+  private api = new RaceApi()
 
-    constructor(private state: GarageState){
-        this.state = state
-    }
+  constructor(private state: GarageState) {
+    this.state = state
+  }
 
-    public async getCars(): Promise<{ cars: Car[]; totalCount: string | null; page: number } | undefined> {
-        try {
-            const response = await this.api.getCars(this.state.currentPage, this.state.length);
-            
-            if (this.isValidCarResponse(response)) {
-                return response;
-            } else {
-                throw new Error("Invalid car data received");
-            }
-        } catch (error) {
-            console.error("Error fetching cars:", error);
-            return undefined;
-        }
-    }
+  public async getCars(): Promise<
+    { cars: Car[]; totalCount: string | null; page: number } | undefined
+  > {
+    try {
+      const response = await this.api.getCars(
+        this.state.currentPage,
+        this.state.length
+      )
 
-    
-    private isValidCarResponse(response: any): response is { cars: Car[]; totalCount: string | null; page: number } {
-        return response && 
-               response.cars && 
-               (typeof response.totalCount === "string" || response.totalCount === null) && 
-               typeof response.page === "number";
+      if (this.isValidCarResponse(response)) {
+        return response
+      }
+      throw new Error("Invalid car data received")
+    } catch (error) {
+      console.error("Error fetching cars:", error)
+      return undefined
     }
+  }
+
+  private isValidCarResponse(
+    response: any
+  ): response is { cars: Car[]; totalCount: string | null; page: number } {
+    return (
+      response &&
+      response.cars &&
+      (typeof response.totalCount === "string" ||
+        response.totalCount === null) &&
+      typeof response.page === "number"
+    )
+  }
 }
