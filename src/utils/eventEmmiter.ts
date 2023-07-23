a19bf344503be9d7effc1129a953745630dbe05c
Anthony <alwaysadevzero@gmail.com>
2023-07-23 11:57:23 +0000

    refactor: rewrite eventEmmiter types

--- src/utils/eventEmmiter.ts
+++ src/utils/eventEmmiter.ts
@@ -1,21 +1,18 @@
 import { Events } from "../enums/enum-events"
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Listener = (args: any) => void
-
 export default class EventEmitter {
-  private listeners: Record<string, Listener[]> = {}
+  private listeners: Record<string, Function[]> = {}
 
   public events: typeof Events = Events
 
-  public on(event: Events, listener: Listener): void {
+  public on(event: Events, listener: Function): void {
     if (!this.listeners[event]) {
       this.listeners[event] = []
     }
     this.listeners[event].push(listener)
   }
 
-  public off(event: Events, listener: Listener): void {
+  public off(event: Events, listener: Function): void {
     if (!this.listeners[event]) return
     const callbackIndex = this.listeners[event].indexOf(listener)
     if (callbackIndex >= 0) {
