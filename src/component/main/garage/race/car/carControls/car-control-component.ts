5df2d4c0212ba30c502a004b7d01d3c8f06f7997
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-24 01:12:51 +0400

    feat: add car control component

--- src/component/main/garage/race/car/carControls/car-control-component.ts
+++ src/component/main/garage/race/car/carControls/car-control-component.ts
@@ -1,5 +1,40 @@
-import BaseComponent from "../../../../../../utils/baseComponent";
+import styles from "./car-control.module.css"
+import BaseComponent from "../../../../../../utils/baseComponent"
 
 export default class CarControlComponent extends BaseComponent {
-    
+  private buttonStart!: BaseComponent
+
+  private buttonStop!: BaseComponent
+
+  private buttonEdit!: BaseComponent
+
+  private buttonReset!: BaseComponent
+
+  constructor() {
+    super({ className: styles.CarControlComponent })
+    this.initComponent()
+  }
+
+  private initComponent = () => {
+    this.buttonStart = new BaseComponent<"button">({
+      tag: "button",
+      content: "start",
+      parent: this.node,
+    })
+    this.buttonStop = new BaseComponent<"button">({
+      tag: "button",
+      content: "stop",
+      parent: this.node,
+    })
+    this.buttonEdit = new BaseComponent<"button">({
+      tag: "button",
+      content: "edit",
+      parent: this.node,
+    })
+    this.buttonReset = new BaseComponent<"button">({
+      tag: "button",
+      content: "reset",
+      parent: this.node,
+    })
+  }
 }
