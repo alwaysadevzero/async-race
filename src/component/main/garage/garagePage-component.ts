ff52df409fa6ce52eabdcdeb3f89b4179e298e71
alwaysadevzero <alwaysadevzero@gmail.com>
2023-07-24 01:14:51 +0400

    feat: add all component in garagePage and add styles

--- src/component/main/garage/garagePage-component.ts
+++ src/component/main/garage/garagePage-component.ts
@@ -1,24 +1,28 @@
+import styles from "./garagePage.module.css"
 import BaseComponent from "../../../utils/baseComponent"
 
-import garageEventEmmiter from "../../../services/garage-eventEmmiter"
-import  GaragePresenter   from "../../../presenter/garage-presenter"
+import GaragePresenter from "../../../presenter/garage-presenter"
 import { GarageState } from "../../../interfaces/garage-state.interface"
 import GarageModel from "../../../model/garage-model"
 
 import RaceComponent from "./race/race-component"
+import CarCreatorComponent from "./carCreator/car-creator-component"
+import CarUpdaterComponent from "./carUpdater/car-updater-component"
+import RaceControlsComponent from "./raceControls/race-controls-component"
+
+import PanelComponent from "../../../shared/component/panel/panel-component"
+import NavigationComponent from "../../../shared/component/navigation/navigation-component"
 
 const STATE: GarageState = {
   currentPage: 1,
   length: 7,
 }
 
-export class GarageComponent extends BaseComponent {
-
+export class GarageComponent extends BaseComponent<"article"> {
   constructor() {
-    super({})
+    super({ className: styles.GarageComponent })
     this.initComponent()
     this.initDependinces()
-
   }
 
   private initDependinces = () => {
@@ -27,6 +31,30 @@ export class GarageComponent extends BaseComponent {
   }
 
   private initComponent = () => {
-    const race = new RaceComponent({parent: this.node})
+    const wrapper = new BaseComponent({
+      parent: this.node,
+      className: styles.wrapper,
+    })
+    const panel = new PanelComponent()
+    wrapper.append(panel)
+    const controlsWrapper = new BaseComponent({
+      parent: wrapper.node,
+      className: styles.controlsWrapper,
+    })
+
+    const raceControls = new RaceControlsComponent()
+    const navigation = new NavigationComponent()
+    controlsWrapper.append(raceControls, navigation)
+
+    const creatorWrapper = new BaseComponent({
+      parent: wrapper.node,
+      className: styles.creatorWrapper,
+    })
+    const carCreator = new CarCreatorComponent()
+    const carUpdater = new CarUpdaterComponent()
+    creatorWrapper.append(carCreator, carUpdater)
+
+    const race = new RaceComponent()
+    this.append(race.node)
   }
 }
